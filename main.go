bbc3eaa60f4dd0c7b14eb18675c8a1fcf5e37e23
Semen <klyuyev.semn@internet.ru>
2026-02-11 18:09:16 +0300

    New fich

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func getText() {
+	var str string
+	fmt.Print("Введите текст: ")
+	fmt.Scan(&str)
+
+	fmt.Println("Вы ввели данный текст:",str)
+}
+
 func main() {
 	const UsdEur = 0.85
 	const UsdRub = 76.79
@@ -9,16 +17,9 @@ func main() {
 
 	fmt.Println(EurRub)
 
-	var str string
-	fmt.Print("Введите текст: ")
-	fmt.Scan(&str)
+	getText()
+}
 
-	fmt.Println("Вы ввели данный текст:",str)
+func convertCurrency (number float64,currency1 string,currency2 string) float64{
 
-	
-
-	}
-
-	func convertCurrency (number float64,currency1 string,currency2 string) float64 {
-		
-	}
+}
