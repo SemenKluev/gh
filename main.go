aca523226e2c5de47b4fd1fafad65e0612ca08d7
Semen <klyuyev.semn@internet.ru>
2026-02-11 16:20:29 +0300

    My test commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	const UsdEur = 0.85
+	const UsdRub = 76.79
+	const EurRub = UsdRub / UsdEur
+
+	fmt.Println(EurRub)
+
+/*	var str string
+	fmt.Print("Введите текст: ")
+	fmt.Scan(&str)
+
+	fmt.Println("Вы ввели данный текст:",str)
+*/
+	}
